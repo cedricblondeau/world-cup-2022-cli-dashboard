33f0c40a151655fcca371b2d3689ec83dfa6eaeb
Cedric Blondeau <hi@cedricblondeau.com>
2022-11-22 12:01:07 +0100

    Use football-data.org api if FOOTBALLDATA_API_TOKEN env variable is set

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cedricblondeau/world-cup-2022-cli-dashboard/data/footballdata"
 	"github.com/cedricblondeau/world-cup-2022-cli-dashboard/data/worldcupjson"
 	"github.com/cedricblondeau/world-cup-2022-cli-dashboard/ui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(ui.NewDashboard(worldcupjson.NewClient()))
+	token, ok := os.LookupEnv("FOOTBALLDATA_API_TOKEN")
+	var dashboard tea.Model
+	if ok {
+		dashboard = ui.NewDashboard(footballdata.NewClient(token))
+	} else {
+		dashboard = ui.NewDashboard(worldcupjson.NewClient())
+	}
+
+	p := tea.NewProgram(dashboard)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Oh no, there's been an error: %v", err)
 		os.Exit(1)
