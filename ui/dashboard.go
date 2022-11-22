e82b85dd45f891f4b57dd9526797da03ea027a48
Brian Kimmig <bkimmig@users.noreply.github.com>
2022-11-22 13:48:47 -0700

    Update ui/dashboard.go
    Co-authored-by: Cédric Blondeau <cedricblondeau@users.noreply.github.com>

--- ui/dashboard.go
+++ ui/dashboard.go
@@ -173,8 +173,8 @@ func (m *dashboard) View() string {
 		String()
 
 	keyMap := keyMap{
-		Left:  key.NewBinding(key.WithKeys("left", "a", "h"), key.WithHelp("◄/a", "prev match")),
-		Right: key.NewBinding(key.WithKeys("right", "d", " ", "l"), key.WithHelp("►/d/space", "next match")),
+		Left:  key.NewBinding(key.WithKeys("left", "a", "h"), key.WithHelp("◄/a/h", "prev match")),
+		Right: key.NewBinding(key.WithKeys("right", "d", " ", "l"), key.WithHelp("►/d/l/space", "next match")),
 		Quit:  key.NewBinding(key.WithKeys("q", "ctrl+c"), key.WithHelp("q/ctrl+c", "quit")),
 	}
 	helpContainer := lipgloss.NewStyle().
